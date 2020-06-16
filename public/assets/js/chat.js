7de9f94a253b0c525fdc00ec3653a69cfc6d65b3
Oskar Jönsson <peroskarjonsson@gmail.com>
2020-06-16 13:29:53 +0200

    added new updated client.js (chat.js) code that is implemented after HTML-code

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -1,144 +1,175 @@
 const socket = io();
 
-const startEl = document.querySelector('#start');
-const chatWrapperEl = document.querySelector('#chat-wrapper');
-const usernameForm = document.querySelector('#username-form');
-const messageForm = document.querySelector('#message-form');
-const gameBoard = document.querySelector('#gameboard');
-const startMatch = document.querySelector('#startMatch')
+const startPage = document.querySelector('#startPage');
+const playerNickname = document.querySelector('#playerNickname-form');
+const lobbyRoom = document.querySelector('#waitForConnect');
+const playingField = document.querySelector('#playingField');
+const gameBoard = document.querySelector('#gameBoard');
+const virus = document.getElementById('virus');
 
+const timer = document.querySelector('#countdown')
 
-// get username from form and emit `register-user`-event to server
-usernameForm.addEventListener('submit', e => {
-	e.preventDefault();
-
-	username = document.querySelector('#username').value;
-	socket.emit('register-user', username, (status) => {
-		console.log("Server acknowledged the registration :D", status);
-
-		if (status.joinChat) {
-			startEl.classList.add('hide');
-			chatWrapperEl.classList.remove('hide');
-
-			updateOnlineUsers(status.onlineUsers);
-		}
-	});
-});
-
-socket.on('online-users', (users) => {
-	updateOnlineUsers(users);
-});
-
-const getRandomPosition = (element) => {
-	var x = document.querySelector("#gameboard").offsetHeight-element.clientHeight;
-	var y = document.querySelector("#gameboard").offsetWidth-element.clientWidth;
-	var randomX = Math.floor(Math.random()* x);
-	var randomY = Math.floor(Math.random()* y);
-	return [randomX,randomY];
+let nickname = null;
+let playersLob = []
+let playerScoreOne = {
+    score: 0,
 }
-
-const addvirus = () => { 
-	var img = document.createElement('img');
-	img.setAttribute("style", "position:absolute;");
-	img.setAttribute("src", "../assets/images/virus.png");
-	document.querySelector("#gameboard").appendChild(img);
-	var xy = getRandomPosition(img);
-	img.style.top = xy[0] + 'px';
-	img.style.left = xy[1] + 'px';
-};
-
-let startClick; 
-let stopClick; 
+let playerScoreTwo = {
+    score: 0,
+}
+let startTime;
+let endTime;
 let reactionTime;
 let score = 1;
 
-startMatch.addEventListener('click', e =>{
-	e.target.remove()
-	setTimeout(function() {
-		addvirus();
-		stopClick=Date.now();
-	}, 1500);
+
+// GENERAL FUNCTIONS
+
+const gameOver = () => {
+    gameBoard.classList.add('hide')
+    playingField.innerHTML = `
+    <div>
+        <h2>Game Over</h2>
+        <h3>Result:</h3>
+        <p>${playersLob[0]}: ${playerScoreOne.score}</p>
+        <p>${playersLob[1]}: ${playerScoreTwo.score}</p>
+    </div>
+    `
+
+}
+const lobby = () => {
+    lobbyRoom.classList.add('hide');
+    playingField.classList.remove('hide');
+}
+
+const updatePlayersOnline = (players) => {
+    console.log(players)
+    playersLob = players;
+    console.log('playersLob', playersLob)
+	document.querySelector('#players-online').innerHTML = players.map(player => `<li class="player">${player}</li>`).join("");
+}
+
+const scoreBoard = (gameData) => {
+    if (gameData.nickname === playersLob[0]) {
+        playerScoreOne.score ++;
+        console.log("playerScoreOne", playerScoreOne)
+
+        const playerOneScore = document.querySelector('#playerOne');
+        playerOneScore.innerHTML = 
+        `<div>
+            <h3>Player One</h3>
+            <p>Score: ${gameData.score}</p>
+            <p>Reactiontime: ${gameData.reaction}</p>
+        </div>`
+    } else if (gameData.nickname === playersLob[1]){
+        playerScoreTwo.score ++;
+        console.log("playerScoreTwo", playerScoreTwo)
+        const playerTwoScore = document.querySelector('#playerTwo');
+        playerTwoScore.innerHTML = 
+        `<div>
+            <h3>Player Two</h3>
+            <p>Score: ${gameData.score}</p>
+            <p>Reactiontime: ${gameData.reaction}</p>
+        </div>`
+    }
+}
+
+var timeleft = 10;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    startTime = Date.now() - 5000;
+    document.getElementById("countdown").innerHTML = "GO";
+  } else {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
+
+const randomVirusPosition = (target) => {
+    virus.classList.add('hide')
+    setTimeout(() => {
+        virus.style.top = target.width + "px";
+        virus.style.left = target.height + "px";
+        virus.classList.remove('hide')
+    }, 1000)
+    startTime = Date.now();
+}
+
+const clickedVirus = (e) => {
+    if(e.target.tagName === 'IMG' ){
+        const score = document.querySelector('#sek')
+ 
+        //stop the timer
+        endTime = Date.now()
+        //reaction time
+        reactionTime = (endTime - startTime - 1000)/1000;
+ 
+        // set reaction time on webpage
+        // score.innerHTML = `<div><h4>${reactionTime} sek</h4></div>`
+    }
+}
+
+/* Start new round */
+const startRound = (clickVirusPosition) => {
+    randomVirusPosition(clickVirusPosition);
+}
+
+
+// EVENT FUNCTIONS
+virus.addEventListener('click', e => {
+    clickedVirus(e);
+    const data = {
+        name: nickname,
+        reaction: reactionTime,
+        score: score++
+    }
+
+	socket.emit('player-click', data)
 })
 
-gameBoard.addEventListener('click', e => { 
-	gameImg = document.querySelector("img")
-	console.log(e.target);
 
-	if (e.target !== gameImg) {
-		console.log("NOT A VIRUS")
-	} else {
-		e.target.remove();
-		startClick=Date.now();
-		reactionTime=(startClick-stopClick)/1000;
-		addScore = score ++;
-		document.getElementById("reactionTime").innerHTML += `<li>${reactionTime} seconds</li>`
-		document.getElementById("scores").innerHTML = `<li id="yourScore">${addScore}</li>`
-
-		setTimeout(function() {
-		addvirus();
-		stopClick=Date.now();
-		}, Math.floor(Math.random() * 5000) + 3000);
-	}
-});
-
-let username = null;
-
-const addNoticeToChat = (notice) => {
-	const noticeEl = document.createElement('li');
-	noticeEl.classList.add('list-group-item', 'list-group-item-light', 'notice');
-
-	noticeEl.innerHTML = notice;
-
-	document.querySelector('#messages').appendChild(noticeEl);
-}
-
-const addMessageToChat = (msg, ownMsg = false) => {
-	const msgEl = document.createElement('li');
-	msgEl.classList.add('list-group-item', 'message');
-	msgEl.classList.add(ownMsg ? 'list-group-item-primary' : 'list-group-item-secondary');
-
-	const username = ownMsg ? 'You' : msg.username;
-	msgEl.innerHTML = `<span class="user">${username}</span>: ${msg.content}`;
-
-	document.querySelector('#messages').appendChild(msgEl);
-}
-
-const updateOnlineUsers = (users, i) => {
-	console.log(users)
-	document.querySelector('#online-users').innerHTML = users.map(user => `<li class="user">Player: ${user}</li>`).join("");
-}
-
-messageForm.addEventListener('submit', e => {
+playerNickname.addEventListener('submit', e => {
 	e.preventDefault();
 
-	const messageEl = document.querySelector('#message');
-	const msg = {
-		content: messageEl.value,
-		username: document.querySelector('#username').value,
-	}
+	nickname = document.querySelector('#nickname').value;
+	socket.emit('register-player', nickname, (status) => {
 
-	socket.emit('chatmsg', msg);
-	addMessageToChat(msg, true);
+        if (status.joinGame) {
+			startPage.classList.add('hide');
+			lobbyRoom.classList.remove('hide');
 
-	messageEl.value = '';
+			updatePlayersOnline(status.onlinePlayers);
+		}
+	});
+
 });
 
+
+
+// SOCKET FUNCTIONS
 socket.on('reconnect', () => {
-	if (username) {
-		socket.emit('register-user', username, () => {
-			console.log("The server acknowledged our reconnect.");
+	if (nickname) {
+		socket.emit('register-player', nickname, () => {
 		});
 	}
 });
 
-socket.on('new-user-connected', (username) => {
-	addNoticeToChat(`${username} connected to the chat 🥳!`);
+socket.on('players-online', (players) => {
+	updatePlayersOnline(players);
 });
 
-socket.on('user-disconnected', (username) => {
-	addNoticeToChat(`${username} left the chat 😢...`);
+
+// socket.on('player-click', (target, gameData) => {
+// });
+
+socket.on('new-round', (clickVirusPosition, gameData) => {
+    scoreBoard(gameData)
+    startRound(clickVirusPosition)
 });
 
-socket.on('chatmsg', (msg) => {
-	addMessageToChat(msg);
-});
+socket.on('game-over', () => {
+    gameOver()
+})
+
+socket.on('create-game-page', lobby);
